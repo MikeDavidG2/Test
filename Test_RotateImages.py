7de3bd3f192432c205631d615d17d61006d33c51
Mike Grue <MikeDavidG2@yahoo.com>
2017-02-07 16:07:24 -0800

    Adding any changes to Test scripts

--- Test_RotateImages.py
+++ Test_RotateImages.py
@@ -9,16 +9,31 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 
-import Image, os
+from PIL import Image
+import os
 
 def main():
     orig_jpg_path = r'U:\grue\Scripts\GitHub\DPW-Sci-Monitoring\Data\Sci_Monitoring_pics'
     prod_jpg_path = r'U:\grue\Scripts\GitHub\DPW-Sci-Monitoring\Data\Sci_Monitoring_pics_rotated'
+
     Rotate_Images(orig_jpg_path, prod_jpg_path)
+    ##Rotate_Images1(orig_jpg_path, prod_jpg_path)
 
 def Rotate_Images(orig_jpg_path, prod_jpg_path):
+    img_filename = '%s/PEN-009__3_A.jpg' % orig_jpg_path
+    img_obj = Image.open(img_filename)
+    img_obj.open(img_filename)
+    im.show()
+
+
+
+
+
+def Rotate_Images1(orig_jpg_path, prod_jpg_path):
+    print os.curdir
     images = os.listdir(orig_jpg_path)
     os.chdir(orig_jpg_path)
+    print os.curdir
     for image in images:
         if image.endswith('.jpg'):
             print 'Image: ' + image
