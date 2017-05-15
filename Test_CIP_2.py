427100620ed75a0316cc23af04eadf3bed982634
Mike Grue <MikeDavidG2@yahoo.com>
2017-05-15 09:43:17 -0700

    Wrote Update_Fields(), working

--- Test_CIP_2.py
+++ Test_CIP_2.py
@@ -19,14 +19,24 @@ def main():
     # FGDB to import the Excel table info
     FGDB_path = r'P:\CIP\20170403_CIP_to_App\Data\CIP_Imported_Excel.gdb'
 
-    # Excel file info
-    excel_file      = r'P:\CIP\20170403_CIP_to_App\Working\CIP_5YEAR_POLY.xlsx'
+    # Update Excel file info
+    excel_file      = r'P:\CIP\20170403_CIP_to_App\Working\Test\CIP_5YEAR_POLY_testing.xlsx'
     sheet_to_import = 'CIP_5YEAR_POLY'
     join_field      = 'PROJECT_ID'
 
     # SDW connection
     sdw_connection  = r'P:\CIP\20170403_CIP_to_App\Data\Fake_SDW.gdb'
-    sdw_cip_fc      = os.path.join(sdw_connection, 'CIP_5YEAR_POLY')
+    sdw_cip_fc_name = 'CIP_5YEAR_POLY'
+    sdw_cip_fc_path      = os.path.join(sdw_connection, sdw_cip_fc_name)
+
+    # List of Fields to update in SDW/SDE Feature Class
+    sdw_field_ls =   ['PROJECT_ID', 'NAME', 'TYPE', 'PROJECT_STATUS',
+                      'DETAIL_WK_PROG', 'FIVE_YR_PLAN', 'EST_START', 'EST_COMPLT',
+                      'EST_PR_CST', 'FUNDING_STATUS', 'FUNDING', 'LENGTH',
+                      'PLANNING_GROUP', 'SUPERVISOR_DISTRICT', 'THOMAS_BROTHERS',
+                      'PROJECT_MANAGER', 'PM_EMAIL', 'PM_PHONE', 'ORACLE_NUMBER',
+                      'DESCRIPTION']
+
 
     #---------------------------------------------------------------------------
     #                       Start calling FUNCTIONS
@@ -38,21 +48,40 @@ def main():
     imported_table = os.path.join(FGDB_path, sheet_to_import + '_' + dt_to_append)
     myFunc.Excel_To_Table(excel_file, imported_table, sheet_to_import)
 
+    # Validate the imported table data (make sure it has the correct fields
+    Validate_Data()
+
     # Process table
     Process_Table()
 
     # Join processed table to SDW CIP Feature Class
-    joined_obj = myFunc.Join_2_Objects(sdw_cip_fc, join_field, imported_table, join_field)
+    joined_fc = myFunc.Join_2_Objects(sdw_cip_fc_path, join_field, imported_table, join_field)
 
-    #TODO: remove this print statement when complete with the script
-    fields = arcpy.ListFields(joined_obj)
-    for field in fields:
-        print field.name
+    # Update fields from imported table to SDW Feature Class
+    Update_Fields(joined_fc, sdw_cip_fc_name, imported_table, sdw_field_ls)
 
 
 #-------------------------------------------------------------------------------
+#*******************************************************************************
 #-------------------------------------------------------------------------------
 #                          Start defining FUNCTIONS
+#-------------------------------------------------------------------------------
+#*******************************************************************************
+#-------------------------------------------------------------------------------
+#                          FUNCTION: Validate_Data()
+def Validate_Data():
+    """
+    """
+
+    print 'Validating Data...'
+
+
+
+    print 'Finished Validating Data\n'
+
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+#                         FUNCTION: Process_Table()
 
 def Process_Table():
     """
@@ -63,6 +92,32 @@ def Process_Table():
 
 
     print 'Finished Processing Table\n'
+
+#-------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+#                         FUNCTION: Update_Fields()
+
+def Update_Fields(joined_fc, sdw_cip_fc_name, imported_table, sdw_field_ls):
+    """
+    """
+
+    print 'Updating Fields in Joined Feature Class...'
+
+    # Get the basename of the imported table, i.e. "CIP_5YEAR_POLY_2017_5_15__9_38_50"
+    # Will be used in 'expression' below
+    import_table_name = os.path.basename(imported_table)
+
+    for field in sdw_field_ls:
+
+        field_to_calc = '{}.{}'.format(sdw_cip_fc_name, field)
+        expression    = '!{}.{}!'.format(import_table_name, field)
+
+        print '  In joined_fc, calculating field: "{}", to equal: "{}"'.format(field_to_calc, expression)
+        arcpy.CalculateField_management(joined_fc, field_to_calc, expression, 'PYTHON_9.3')
+
+
+
+    print 'Finished Updating Fields\n'
 
 #-------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------
