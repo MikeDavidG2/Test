613f5be6b0ee049deb1a970f6be0d7055e795339
Mike Grue <MikeDavidG2@yahoo.com>
2017-05-16 15:52:39 -0700

    Documenting some of the functions

--- myFunc.py
+++ myFunc.py
@@ -6,6 +6,11 @@ import os, arcpy, datetime
 
 def Create_FGDB(path_name_FGDB, overwrite_if_exists=False):
     """
+    PARAMETERS:
+
+    RETURNS:
+
+    FUNCTION:
     """
 
     print 'Starting Create_FGDB()...'
@@ -45,6 +50,11 @@ def Create_FGDB(path_name_FGDB, overwrite_if_exists=False):
 #                                 FUNCTION Copy_FC()
 def Copy_FC(in_features, out_feature_class):
     """
+    PARAMETERS:
+
+    RETURNS:
+
+    FUNCTION:
     """
 
     print 'Starting Copy_FC()...'
@@ -60,6 +70,11 @@ def Copy_FC(in_features, out_feature_class):
 #                                 FUNCTION Copy_Rows()
 def Copy_Rows(in_table, out_table):
     """
+    PARAMETERS:
+
+    RETURNS:
+
+    FUNCTION:
     """
 
     print 'Starting Copy_Rows()...'
@@ -74,6 +89,20 @@ def Copy_Rows(in_table, out_table):
 #-------------------------------------------------------------------------------
 #                       FUNCTION Excel_To_Table()
 def Excel_To_Table(input_excel_file, out_table, sheet):
+    """
+    PARAMETERS:
+        input_excel_file (str): The full path to the Excel file to import.
+
+        out_table (str): The full path to the FGDB and NAME of the table in the FGDB.
+
+        sheet (str): The name of the sheet to import.
+
+    RETURNS:
+        none
+
+    FUNCTION:
+        To import an Excel sheet into a FGDB.
+    """
 
     print 'Starting Excel_To_Table()...'
 
@@ -88,10 +117,30 @@ def Excel_To_Table(input_excel_file, out_table, sheet):
 #                       FUNCTION Select_Object()
 def Select_Object(path_to_obj, selection_type, where_clause):
     """
+    PARAMETERS:
+      path_to_obj (str): Full path to the object (Feature Layer or Table) that
+        is to be selected.
+
+      selection_type (str): Selection type.  Valid values are:
+        NEW_SELECTION
+        ADD_TO_SELECTION
+        REMOVE_FROM_SELECTION
+        SUBSET_SELECTION
+        SWITCH_SELECTION
+        CLEAR_SELECTION
+
+      where_clause (str): The SQL where clause.
+
+    RETURNS:
+      'lyr' (lyr): The layer/view with the selection on it.
+
+    FUNCTION:
+      To perform a selection on the object.
     """
 
     print 'Starting Select_Object()...'
 
+    # Use try/except to handle either object type (Feature Layer / Table)
     try:
         arcpy.MakeFeatureLayer_management(path_to_obj, 'lyr')
     except:
@@ -107,11 +156,22 @@ def Select_Object(path_to_obj, selection_type, where_clause):
 #-------------------------------------------------------------------------------
 #                        FUNCTION Get_Count_Selected()
 def Get_Count_Selected(lyr):
+    """
+    PARAMETERS:
+      lyr (lyr): The layer that should have a selection on it that we want to test.
+
+    RETURNS:
+      count_selected (int): The number of selected records in the lyr
+
+    FUNCTION:
+      To get the count of the number of selected records in the lyr.
+    """
 
     print 'Starting Get_Count()...'
 
     # See if there are any selected records
     desc = arcpy.Describe(lyr)
+
     if desc.fidSet: # True if there are selected records
         result = arcpy.GetCount_management(lyr)
         count_selected = int(result.getOutput(0))
@@ -132,6 +192,15 @@ def Get_Count_Selected(lyr):
 
 def Get_DT_To_Append():
     """
+    PARAMETERS:
+      none
+
+    RETURNS:
+      dt_to_append (str): Which is in the format 'YYYY_M_D__H_M_S'
+
+    FUNCTION:
+      To get a formatted datetime string that can be used to append to files
+      to keep them unique.
     """
     print 'Starting Get_DT_To_Append()...'
 
@@ -153,6 +222,29 @@ def Get_DT_To_Append():
 
 def Join_2_Objects(target_obj, target_join_field, to_join_obj, to_join_field):
     """
+    PARAMETERS:
+      target_obj (str): The full path to the FC or Table that you want to have
+        another object join to.
+
+      target_join_field (str): The field name in the target_obj to be used as the
+        primary key.
+
+      to_join_obj (str): The full path to the FC or Table that you want to join
+        to the target_obj.
+
+      to_join_field (str): The field name in the to_join_obj to be used as the
+        foreign key.
+
+    RETURNS:
+      target_obj (lyr): Return the layer/view of the joined object so that
+        it can be processed.
+
+    FUNCTION:
+      To join two different objects via a primary key field and a foreign key
+      field by:
+        1) Creating a layer or table view for each object ('target_obj', 'to_join_obj')
+        2) Joining the layer(s) / view(s) via the 'target_join_field' and the
+           'to_join_field'
     """
 
     print 'Starting Join_2_Objects()...'
