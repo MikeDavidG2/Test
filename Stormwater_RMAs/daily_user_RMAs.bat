fdcaf6c7ed8ffd8ed3b2e081175f331594fb3724
Mike Grue <MikeDavidG2@yahoo.com>
2017-06-27 14:19:48 -0700

    changed bat file to call the DEV script

--- Stormwater_RMAs/daily_user_RMAs.bat
+++ Stormwater_RMAs/daily_user_RMAs.bat
@@ -1,1 +1,1 @@
-C:\Python27\ArcGIS10.4\python.exe D:\Projects\stormwater\scripts\daily_user_RMAs.py
+C:\Python27\ArcGIS10.4\python.exe U:\grue\Scripts\GitHub\Test\Stormwater_RMAs\daily_user_RMAs.bat
